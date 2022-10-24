46a119d01e19315ddfb9c8e6f900b9db5018406e
cedric ngakam <cedricnga2@gmail.com>
2022-10-24 10:16:55 +0200

    wip

--- contracts/DNFT0.sol
+++ contracts/DNFT0.sol
@@ -17,11 +17,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./RandomNumber.sol";
 import "./interfaces/IDNFT.sol";
 
-
-//todo : grammaire( _ internal, majuscules etc), commentaires
 
 contract DNFT0 is ERC721{
 
